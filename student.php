b987d5e66ea6093b9cb47f101a33a79755fb6b8a
Victor Mercado <victormercado@outlook.com>
2023-11-14 15:14:01 -0800

    Fixed function

--- student.php
+++ student.php
@@ -1,15 +1,15 @@
 <?php
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the professor's SSN from the form
+    // Get the student's CWID from the form
     $CWIDNo = $_POST["CWIDNo"];
 
     // Include the function from the previous example
-    include "controllers.php"; // Replace with the actual filename
+    include "controllers.php"; 
 
     ob_start();
     // Call the function with the provided SSN
-    getClassInformationForProfessor($CWIDNo);
+    studentClasses($CWIDNo);
     $output = ob_get_clean();
 
     // Display the output
